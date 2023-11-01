59e2324d85810390f1c4a898a45344663dacc159
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-11-01 09:27:49 +0900

    🚀 :: 파일 다중 업로드 API 추가

--- src/upload/dto/upload-response.dto.ts
+++ src/upload/dto/upload-response.dto.ts
@@ -7,3 +7,16 @@ export class UploadResponseDto {
   })
   url: string;
 }
+
+export class UploadMultipleResponseDto {
+  @ApiProperty({
+    description: '업로드된 파일의 URL 목록',
+    type: [String],
+    example: [
+      'https://www.printshop.com/image1.jpg',
+      'https://www.printshop.com/image2.jpg',
+      'https://www.printshop.com/image3.jpg',
+    ],
+  })
+  urls: string[];
+}
--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -2,10 +2,11 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { CloudflareService } from './cloudflare.service';
 import {
   ApiBody,
@@ -16,7 +17,10 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { UploadResponseDto } from './dto/upload-response.dto';
+import {
+  UploadMultipleResponseDto,
+  UploadResponseDto,
+} from './dto/upload-response.dto';
 import { v4 as uuidv4 } from 'uuid';
 
 @UseGuards(AuthGuard('jwt'))
@@ -58,5 +62,43 @@ export class UploadController {
       uniqueFileName,
     );
     return { url };
+  }
+
+  @Post('multiple')
+  @ApiOperation({
+    summary: '파일 다중 업로드',
+    description: '파일을 다중 업로드하는 API입니다.',
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'array',
+          items: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+      },
+    },
+  })
+  @ApiOkResponse({
+    description: '파일 다중 업로드 성공',
+    type: UploadMultipleResponseDto,
+  })
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    const urls = [];
+    for (const file of files) {
+      const uniqueFileName = `${uuidv4()}_${file.originalname}`;
+      const url = await this.cloudflareService.resizeAndUploadFile(
+        file.buffer,
+        uniqueFileName,
+      );
+      urls.push(url);
+    }
+    return { urls };
   }
 }
