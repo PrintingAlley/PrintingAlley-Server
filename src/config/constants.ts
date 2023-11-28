873cfa299ec0a937f0759b49eeef348cc2099aad
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-11-28 09:01:40 +0900

    🛠️ :: 버전 비교 함수 수정

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,7 @@
 export const DEFAULT_WIDTH = 1920;
 
-export const API_VERSION = 'v1.0.0';
+export const API_CURRENT_VERSION = 'v1.0.1';
+export const API_MINIMUM_VERSION = 'v1.0.0';
 
 export const PRINT_TYPE_TAG_NAME = '인쇄 방식';
 export const AFTER_PROCESS_TAG_NAME = '후가공';
--- src/version/version.service.ts
+++ src/version/version.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { API_VERSION } from 'src/config/constants';
+import { API_CURRENT_VERSION, API_MINIMUM_VERSION } from 'src/config/constants';
 
 @Injectable()
 export class VersionService {
   checkVersion(version: string): number {
-    if (version !== API_VERSION) {
-      return 2;
-    } else {
+    if (version >= API_CURRENT_VERSION) {
       return 0;
     }
+    if (version >= API_MINIMUM_VERSION) {
+      return 1;
+    }
+    return 2;
   }
 }
